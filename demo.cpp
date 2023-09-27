14c054eb1be31f9669448264ed51d3fce1a98f13
chucrutt <129561782+chucrutt@users.noreply.github.com>
2023-09-27 00:31:21 +0000

    26/09

--- demo.cpp
+++ demo.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World" << endl;
+    // Tamaño del laberinto
+    int n;
+    cin >> n;
+    // Inicializar el laberinto
+    vector<vector<pair<int,bool>>> dp(n, vector<pair<int,bool>> (n, make_pair(0, false)));
+    // vector<vector<int>> dp(n, vector<int>(n, 0));
+    string s;
+    for (int i=0; i<n-1; i++) {
+        cin >> s;
+        for (int j=0; j<n-1; j++) {
+            if (s[j] == '*') {
+                dp[i][j] = make_pair(0, true);
+            }
+        }
+    }
     return 0;
 }
